0d190e4b214c15d0aab8f65cb21da8d8dbfaddeb
Sergey <nedzelskiysergey@yandex.ru.com>
2026-02-02 22:17:59 +0300

    big fixes build inline bot

--- backend/bot/__init__.py
+++ backend/bot/__init__.py
@@ -1,4 +1,4 @@
 """Bot package"""
-from backend.bot.bot import bot, dp, get_bot
+from backend.bot.bot import bot, get_bot, init_bot_for_notifications, close_bot
 
-__all__ = ["bot", "dp", "get_bot"]
+__all__ = ["bot", "get_bot", "init_bot_for_notifications", "close_bot"]
